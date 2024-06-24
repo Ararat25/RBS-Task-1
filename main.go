04ff80acc2f0117c0c585ef50a7d1ef001329d95
Ararat Vanyan <ararat_vanyan@bk.ru>
2024-06-25 02:14:52 +0400

    добавлена проверка флагов, более информативный вывод, комментари к функциям, изменение названий функций и переменных

--- main.go
+++ main.go
@@ -1,89 +1,127 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "flag"
-    "strings"
-    "net/url"
-    "net/http"
-    "io"
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"time"
 )
 
+// scanFile считывает строки из файла и записывает в слайс
 func scanFile(fileName string) ([]string, error) {
-    urls, err := os.ReadFile(fileName)
-    if err != nil {
-        return nil, err
-    }
+	lines, err := os.ReadFile(fileName)
+	if err != nil {
+		return nil, err
+	}
 
-    lines := strings.Split(string(urls), "\n")
+	linesArr := strings.Split(string(lines), "\n")
 
-    return lines, nil
+	return linesArr, nil
 }
 
-func parseUrl(urls []string) ([]*url.URL) {
-    result := []*url.URL{}
+// parseUrl отфильтровывает валидные URL от невалидных и записывает в слайс
+func parseUrl(urls []string) []*url.URL {
 
-    for _, str := range urls {
-        url, err := url.Parse(str)
-        if err == nil && url.Scheme != "" && url.Host != "" {
-            result = append(result, url)
-        }
-    }
+	urlArr := []*url.URL{}
 
-    return result
+	for _, str := range urls {
+		url, err := url.Parse(str)
+		if err == nil && url.Scheme != "" && url.Host != "" {
+			urlArr = append(urlArr, url)
+		}
+	}
+
+	return urlArr
 }
 
-func writeGetResult(urls []*url.URL, outputDirectoryName string) (error){
-    for _, url := range urls {
-        response, err := http.Get(url.String())
-        if err != nil {
-            continue
-        }
+// upload получает данные из URL запросов и записывает в файлы
+func upload(urls []*url.URL, outputDirectoryName string) []string {
+	arrFileName := []string{}
 
-        body, err := io.ReadAll(response.Body)
-        if err != nil {
-            continue
-        }
+	for _, url := range urls {
+		response, err := http.Get(url.String())
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
 
-        content := string(body)
-        
-        fileName := fmt.Sprintf("%s/%s.txt", outputDirectoryName, url.Host)
-        
-        err = os.WriteFile(fileName, []byte(content), 0777)
-        if err != nil {
-            fmt.Println(err)
-            continue
-        }
-    }
+		func() {
+			defer response.Body.Close()
 
-    return nil
+			body, err := io.ReadAll(response.Body)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			content := string(body)
+
+			fileName := fmt.Sprintf("%s/%s.txt", outputDirectoryName, url.Host)
+
+			err = os.WriteFile(fileName, []byte(content), 0777)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			arrFileName = append(arrFileName, fileName)
+		}()
+	}
+
+	return arrFileName
+}
+
+func printFileName(fNames []string) {
+	fmt.Println("\nПути к созданным файлам:")
+	for _, fname := range fNames {
+		fmt.Println(fname)
+	}
 }
 
 func main() {
-    srcPtr := flag.String("src", "", "Название исходного файла")
-    dstPtr := flag.String("dst", "", "Название конечной директории")
+	srcPtr := flag.String("src", "", "Название исходного файла")
+	dstPtr := flag.String("dst", "", "Название конечной директории")
 
-    flag.Parse()
-    
-    inputFileName := *srcPtr
-    outputDirectoryName := *dstPtr
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Использование: go run main.go --src=<путь_к_файлу> --dst=<путь_к_конечной_папке>\n")
+		flag.PrintDefaults()
+	}
 
-    _ = os.MkdirAll(outputDirectoryName, 0777)
-    
-    urls, err := scanFile(inputFileName)
-    if err!= nil {
-        fmt.Println(err)
-        return
-    }
+	flag.Parse()
 
-    result := parseUrl(urls)
+	if *srcPtr == "" || *dstPtr == "" {
+		fmt.Println("Error: missing required flags.")
+		flag.Usage()
+		return
+	}
 
-    err = writeGetResult(result, outputDirectoryName)
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
+	start := time.Now()
+	fmt.Printf("Запуск программы: %v\n\n", start.Format("02.01.06 15:04:05"))
 
-    fmt.Println("Ok")
+	inputFileName := *srcPtr
+	outputDirectoryName := *dstPtr
+
+	err := os.MkdirAll(outputDirectoryName, 0777)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	urls, err := scanFile(inputFileName)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	urlsArray := parseUrl(urls)
+
+	arrFileName := upload(urlsArray, outputDirectoryName)
+
+	printFileName(arrFileName)
+
+	fmt.Printf("\nКонец работы программы: %v\n", time.Now().Format("02.01.06 15:04:05"))
+	fmt.Printf("Общее время работы: %v\n", time.Since(start))
 }
