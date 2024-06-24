7a7d7404ca53d4885de80fd132cdc46c7b07b28e
Ararat Vanyan <ararat_vanyan@bk.ru>
2024-06-24 17:11:30 +0400

    first commit

---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "flag"
+    "strings"
+    "net/url"
+    "net/http"
+    "io"
+)
+
+func scanFile(fileName string) ([]string, error) {
+    urls, err := os.ReadFile(fileName)
+    if err != nil {
+        return nil, err
+    }
+
+    lines := strings.Split(string(urls), "\n")
+
+    return lines, nil
+}
+
+func parseUrl(urls []string) ([]*url.URL) {
+    result := []*url.URL{}
+
+    for _, str := range urls {
+        url, err := url.Parse(str)
+        if err == nil && url.Scheme != "" && url.Host != "" {
+            result = append(result, url)
+        }
+    }
+
+    return result
+}
+
+func writeGetResult(urls []*url.URL, outputDirectoryName string) (error){
+    for _, url := range urls {
+        response, err := http.Get(url.String())
+        if err != nil {
+            return err
+        }
+
+        body, err := io.ReadAll(response.Body)
+        if err != nil {
+            return err
+        }
+
+        content := string(body)
+        
+        fileName := fmt.Sprintf("%s/%s.txt", outputDirectoryName, url.Host)
+        
+        err = os.WriteFile(fileName, []byte(content), 0777)
+    }
+
+    return nil
+}
+
+func main() {
+    srcPtr := flag.String("src", "", "Название исходного файла")
+    dstPtr := flag.String("dst", "", "Название конечной директории")
+
+    flag.Parse()
+    
+    inputFileName := *srcPtr
+    outputDirectoryName := *dstPtr
+
+    _ = os.Mkdir(outputDirectoryName, 0777)
+    
+    urls, err := scanFile(inputFileName)
+    if err!= nil {
+        fmt.Println(err)
+        return
+    }
+
+    result := parseUrl(urls)
+
+    err = writeGetResult(result, outputDirectoryName)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+    fmt.Println("Ok")
+}
